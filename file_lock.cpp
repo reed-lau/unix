f1b2b56e6ca2ed47426192e3769baccabf3649cb
Wei Liu <geoliuwei@gmail.com>
2018-09-24 12:02:29 +0800

    Update file_lock.cpp

--- file_lock.cpp
+++ file_lock.cpp
@@ -2,65 +2,71 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/file.h>
+
+#include <mutex>
 #include <thread>
+
+#include <iostream>
+#include <chrono>
+
+using namespace std::chrono;
+
+class mutex_locker
+{
+  public:
+      mutex_locker() = default;
+      int lock()
+      {
+          mutex_.lock();
+          return 0;
+      }
+
+      int unlock()
+      {
+          mutex_.unlock();
+          return 0;
+      }
+  private:
+      std::mutex mutex_;
+};
 
 class locker
 {
     public:
-        locker() = default;
+        locker()
+        {
+            fd_ = open("lock.lock", O_CREAT | O_EXCL, 0777);
+            fd_ = open("lock.lock", O_RDWR );
+        }
     int lock()
     {
         int ret;
-
-        fd = open("lock.lock", O_CREAT | O_EXCL, 0777);
-
-        if ( fd != -1 )
-            close(fd);
-
-        fd = open("lock.lock", O_RDWR );
-        if ( fd == -1 )
-            return -1;
-
-        ret = flock(fd, LOCK_EX);
-        if ( ret == -1 )
-            return -1;
-
-        //close(fd); 
-
+        ret = flock(fd_, LOCK_EX);
         return 0;
     }
 
     int unlock()
     {
         int ret;
-
-    //    fd = open("lock.lock", O_RDWR );
-        if ( fd == -1 )
-            return -1;
-
-        ret = flock(fd, LOCK_UN);
-        if ( ret == -1 )
-            return -1;
-
-        close(fd); 
-        // unlink("lock.lock");
-
-        return 0;
+        ret = flock(fd_, LOCK_UN);
+        return ret;
     }
 private:
-    int fd;
+    int fd_;
 };
 
 int start = 0;
 long long sum = 0;
 
 
+    mutex_locker l;
 void foo()
 {
-    locker l;
     while(!start)
         ;
 
+
+    auto tic = std::chrono::high_resolution_clock::now();
     for (int i=0; i<=100000; ++i)
     {
 	    l.lock();
@@ -69,11 +75,14 @@ void foo()
   
 	    l.unlock();   
     }
+    auto toc = std::chrono::high_resolution_clock::now();
+
+    auto dur = duration_cast<milliseconds>(toc - tic);
+    std::cout<< dur.count()<< std::endl;
 }
 
 int main(int argc,char *argv[])
 {
-
      start = 0;
 
      std::thread t1(foo);
